4e89a9375a4f8f620e3e43d7502a80641e2a2af8
john benjamin <johnzeus14@gmail.com>
2019-02-17 23:53:34 +0100

    updated views

--- src/router.js
+++ src/router.js
@@ -9,40 +9,40 @@ import Router from 'vue-router'
 
 
 // views
-import home from './views/home.vue'
-import profile from './views/profile.vue'
-import feeds from './views/feeds.vue'
+import home from '@/views/home.vue'
+import profile from '@/views/profile.vue'
+import feeds from '@/views/feeds.vue'
 
 
 
 
 
-const requireAuthenticated = (to, from, next) => {
-  store.dispatch('auth/initialize')
-    .then(() => {
-      if (!store.getters['auth/isAuthenticated']) {
-        next('/login');
-      } else {
-        next();
-      }
-    });
-};
+// const requireAuthenticated = (to, from, next) => {
+//   store.dispatch('auth/initialize')
+//     .then(() => {
+//       if (!store.getters['auth/isAuthenticated']) {
+//         next('/login');
+//       } else {
+//         next();
+//       }
+//     });
+// };
 
-const requireUnauthenticated = (to, from, next) => {
-  store.dispatch('auth/initialize')
-    .then(() => {
-      if (store.getters['auth/isAuthenticated']) {
-        next('/home');
-      } else {
-        next();
-      }
-    });
-};
+// const requireUnauthenticated = (to, from, next) => {
+//   store.dispatch('auth/initialize')
+//     .then(() => {
+//       if (store.getters['auth/isAuthenticated']) {
+//         next('/home');
+//       } else {
+//         next();
+//       }
+//     });
+// };
 
-const redirectLogout = (to, from, next) => {
-  store.dispatch('auth/logout')
-    .then(() => next('/login'));
-};
+// const redirectLogout = (to, from, next) => {
+//   store.dispatch('auth/logout')
+//     .then(() => next('/login'));
+// };
 
 
 Vue.use(Router)
