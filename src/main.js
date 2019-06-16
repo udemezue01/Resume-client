ce73fcccf825688ddc863d3a9f6e02fbb2c650af
john benjamin <johnzeus14@gmail.com>
2019-06-16 01:21:03 +0100

    added support for vue appolo

--- src/main.js
+++ src/main.js
@@ -34,7 +34,32 @@ const options = {
   location: 'top',
   inverse: false
 }
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:3020/graphql',
+})
+
+// Cache implementation
+const cache = new InMemoryCache()
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
+import VueApollo from 'vue-apollo'
+
+Vue.use(VueApollo)
 Vue.use(VueProgressBar, options)
 
 
@@ -43,5 +68,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
