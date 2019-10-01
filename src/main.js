57a397f9bddc5b141b78e0c413d678fa419bb265
john benjamin <johnzeus14@gmail.com>
2019-10-01 20:57:37 +0100

    removed uikit

--- src/main.js
+++ src/main.js
@@ -8,16 +8,14 @@ import axios from 'axios'
 import VueProgressBar from 'vue-progressbar'
 
 import VueSVGIcon from 'vue-svgicon'
-import 'uikit/dist/js/uikit.js'
-import 'uikit/dist/js/uikit-icons.js'
-import './custom-icons'
+
 import Vuikit from 'vuikit'
 //uikit css and javascript
 
 import VueContentPlaceholders from 'vue-content-placeholders'
 Vue.use(Vuikit)
 Vue.use(VueContentPlaceholders)
-import './theme/theme.less'
+
 
 import '@/assets/css/tailwind.css'
 
