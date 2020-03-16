c8f1c06abeebba8d983682400f9a482ebd8f76e6
udemezue John <johnzeus14@gmail.com>
2020-03-16 03:33:27 +0100

    updated landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -15,6 +15,13 @@
       <div>
         
 
+      <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-purple-700 rounded-lg hover:bg-purple-600 focus:outline-none focus:shadow-outline-purple focus:border-purple-600 active:bg-purple-600 transition duration-150 ease-in-out">
+        
+      
+        <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 -ml-1 mr-2"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
+        Get Started
+      </button>
+    
         
 
       </div>
@@ -23,6 +30,21 @@
     </nav>
 
        <!--  End of the navigational bar -->
+
+       <div class="flex">
+
+        <div class="w-1/2">
+          
+        </div>
+
+        <div class="w-1/2">
+          
+          <img src="/img/landing.svg" class="object-center h-full">
+
+        </div>
+         
+
+       </div>
 
 
   </div>
