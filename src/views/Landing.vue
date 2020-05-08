7f95ffa4484a3c1e2ce5c937a15485b363e6a6be
udemezue John <johnzeus14@gmail.com>
2020-05-08 02:34:16 +0100

    added landing

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -9,7 +9,7 @@
 
         <div class="w-full" id="landing">
 
-          <div class="m-6 lg:m-8">
+          <div class="m-6 lg:m-10">
 
             <h1 class="text-6xl text-purple-700 font-black cursor-pointer leading-tight tracking-wide"> Resume.ly</h1>
 
