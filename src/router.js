9ffca159d685c6800d79a9a6cad9746f614945d2
Udemezue John <johnzeus14@gmail.com>
2020-05-20 12:10:29 -0700

    updated packages

--- src/router.js
+++ src/router.js
@@ -11,11 +11,11 @@ import store from './store/store.js'
 //   // ...
 // })
 
-import Profile from './views/Profile.vue';
+import Profile from '@/views/Profile.vue';
 // import Feeds from '@/views/Feeds.vue'
-import Register from './views/Register.vue';
- import Landing from './views/Landing.vue';
-import Login from './views/Login.vue';
+import Register from '@/views/Register.vue';
+ import Landing from '@/views/Landing.vue';
+import Login from '@/views/Login.vue';
 // import notfound from '@/views/404.vue'
 // import job from '@/views/jobs.vue'
 
