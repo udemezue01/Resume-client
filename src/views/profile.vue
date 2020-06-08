dbb67cce99c8fff083b44f51e3e467e502a0c123
Udemezue John <johnzeus14@gmail.com>
2020-06-08 22:54:06 +0100

    updated profile

--- src/views/profile.vue
+++ src/views/profile.vue
@@ -1,88 +1,44 @@
 <template>
- <div class="container  mt-6 h-screen">
+ <div class="container  mt-2 h-screen">
 
          <!--  The Profile UI section -->
 
-    <div class="flex px-5 align-center">
+    <div class="flex px-2">
 
                  <!-- The first section -->
 
-            <div class="w-2/6 ">
+            <div class="w-1/5 bg-white rounded h-full">
 
-                <div class="bg-white border rounded-lg overflow-hidden shadow-sm ">
-                  
-                  <img src="/img/examples.jpg" class="w-full h-64 object-cover object-center">
-
-                  <img src="/img/ali-baba.png" class="border mx-auto rounded-full h-32 w-32 -mt-16" />
-
-                  <div class="p-6 ">
-                    
-                       <h4 class="font-semibold text-center text-gray-800"> Alibaba John</h4>
-                       <p class="font-semibold text-center text-gray-600 tracking-wider">@alibaba</p>
-
-                       <div>
-                         
-                         
-
-                       </div>
-
-                       <div class="mt-12 ">
-                        
-           
-                       </div>
-
-                  </div>
-
-                 
-
-                
-                </div>
-
-               <!--  End of the profile card -->
-
-                <div class="bg-white border rounded-lg shadow-sm mt-6">
-
-                  <div class="p-6">
-                    
-                    <h4 class="text-gray-800 text-center font-semibold rounded-lg hover:bg-purple-300 px-2 py-2"> Details</h4>
-                  </div>
-                  
-
-                </div>
-
-               <!--  End of the second card -->
-               
-                
               
+              <ul class="list-none space-y-6 p-4">
+                
+                <li> <div class="inline-flex">  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24" class="w-6 h-6 text-gray-700" ><path d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"></path></svg><span class="font-semibold text-gray-700 -mr-6"> Feeds</span></div> </li>
+
+                 <li> <div class="inline-flex">  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24" class="w-6 h-6 text-gray-700" ><path d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"></path></svg><span class="font-semibold text-gray-700 -mr-6"> Explore</span></div> </li>
+                 <li> <div class="inline-flex">  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24" class="w-6 h-6 text-gray-700" ><path d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"></path></svg><span class="font-semibold text-gray-700 -mr-6"> Messages</span></div> </li>
+
+                  <li> <div class="inline-flex">  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24" class="w-6 h-6 text-gray-700" ><path d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"></path></svg><span class="font-semibold text-gray-700 -mr-6"> Jobs</span></div> </li>
+
+                   <li> <div class="inline-flex">  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24" class="w-6 h-6 text-gray-700" ><path d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"></path></svg><span class="font-semibold text-gray-700 -mr-6"> Application</span></div> </li>
+
+                    <li> <div class="inline-flex">  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24" class="w-6 h-6 text-gray-700" ><path d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"></path></svg><span class="font-semibold text-gray-700 -mr-8"> Archives</span></div> </li>
+                     <li> <div class="inline-flex">   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6 text-gray-700"><path d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg><span class="font-semibold text-gray-700 -mr-6"> Insights</span></div> </li>
+            
+                       <li> <div class="inline-flex">  <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24" class="w-6 h-6 text-gray-700" ><path d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"></path></svg><span class="font-semibold text-gray-700 -mr-6"> Schedule</span></div> </li>
+            
+            
+                  <li> <div class="inline-flex">   <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6 text-gray-700"><path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg><span class="font-semibold text-gray-700 -mr-6"> Profile</span></div> </li>
+
+              </ul>
 
             </div>
 
                   <!-- End of the first section -->
 
                      <!--    The second section-->
-            <div class="w-3/6">
+            <div class="w-3/5">
 
-              <!-- start of the navbar -->
-
-              <div class="flex justify-between bg-white px-10 py-3 ml-3 rounded-lg shadow-sm">
-
-                <div >
-                  <a href="" class="inline-flex   "> <span class="text-gray-800 font-semibold font-sans"> Post </span></a>
-                </div>
-                <div>
-                   <a href="" class="inline-flex"> <span class="text-gray-800 font-semibold font-sans"> Media </span></a>
-                </div>
-                <div>
-                   <a href="" class="inline-flex"> <span class="text-gray-800 font-semibold font-sans"> Jobs </span></a>
-                </div>
-                <div>
-                   <a href="" class="inline-flex"> <span class="text-gray-800 font-semibold font-sans"> Archive </span></a>
-                </div>
-                
-
-              </div>
-
-           <!--    end of the navbar -->
+             
 
            <!-- The toggle section for the post, jobs and application -->
 
@@ -100,17 +56,9 @@
 
 
                       <!--  The third section -->
-            <div class="w-2/6">
+            <div class="w-1/5">
 
-               <div class="bg-white border rounded-lg shadow-sm ml-4 w-full">
-
-                  <div class="p-6">
-                    
-                    <h4 class="text-gray-800 text-center font-semibold rounded-lg hover:bg-purple-300 px-2 py-2"> Details</h4>
-                  </div>
-                  
-
-                </div>
+              
               
 
             </div>
