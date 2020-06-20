8122656936ab66630e1e1a1b8d725b5d308f4c36
Udemezue John <johnzeus14@gmail.com>
2020-06-20 14:47:02 +0100

    updated profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -7,6 +7,30 @@
 		
 		<img src="/img/woman.jpg" class="w-full h-64 object-center object-cover">
 
+			<div class="flex justify-between mt-6 px-8">
+
+			<!-- 	The profile photo section -->
+
+				<div>
+					
+					<img src="/img/Ali-baba.png" class="w-24 h-24 rounded-full -mt-16">
+
+				</div>
+
+				<!-- End of the profile photo section -->
+
+
+				<div>
+					 <button type="button" class=" mt-6 font-body items-center -ml-1 px-4 py-2  border border-transparent text-sm  font-extrabold text-white bg-purple-700 rounded-lg m-2 tracking-wide lg:block hover:bg-purple-600 focus:outline-none focus:shadow-outline-purple focus:border-purple-600 active:bg-purple-600 transition duration-150 ease-in-out">
+        
+    						Edit Profile
+        				
+      				</button>
+				</div>
+				
+
+			</div>
+
 	</div>
 
 	<!-- End of the user profile card section -->
