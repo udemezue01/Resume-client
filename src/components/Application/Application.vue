4ae54839a24a51e489ecd4bf108e026ad1497db3
Udemezue John <johnzeus14@gmail.com>
2020-07-02 03:15:02 +0100

    updated component width:

--- src/components/Application/Application.vue
+++ src/components/Application/Application.vue
@@ -5,11 +5,11 @@
 
 		<!-- The card section -->
 
-		<div class="bg-white rounded-lg shadow-md w-full lg:w-9/12 lg:mx-auto">
+		<div class="bg-white rounded-lg shadow-md w-full lg:w-7/12 lg:mx-auto">
 
 		<!-- The card header -->
 
-		<div class="flex px-24 mt-4 py-4 justify-between">
+		<div class="flex px-8 mt-4 py-4 justify-between">
 
 			<div class="inline-flex">
 				<span class="font-body font-bold tracking-wider text-gray-700 text-sm"> Application</span>
--- src/components/Jobs/Jobs.vue
+++ src/components/Jobs/Jobs.vue
@@ -3,11 +3,11 @@
 <div id="jobs">
 
 
-	<div class="bg-white rounded-md shadow-md w-full lg:w-9/12 lg:mx-auto">
+	<div class="bg-white rounded-md shadow-md w-full lg:w-7/12 lg:mx-auto">
 
 		<!-- The card header -->
 
-		<div class="flex px-6 mt-4 py-4 justify-between lg:px-24">
+		<div class="flex px-6 mt-4 py-4 justify-between lg:px-8">
 
 			<div class="inline-flex">
 				<span class="font-body font-body font-bold tracking-wider text-gray-700">Jobs</span>
