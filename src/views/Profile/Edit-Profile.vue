6640b4004cf23ddaf03df96230dbb4ec3d6c5366
Udemezue John <johnzeus14@gmail.com>
2020-08-09 16:58:56 +0100

    updated profile section

---
+++ src/views/Profile/Edit-Profile.vue
@@ -0,0 +1,49 @@
+<template>
+	
+<div id="profile" class="p-2 h-full w-full mx-auto lg:w-8/12">
+
+
+	
+
+
+
+
+</div>
+
+
+
+</template>
+
+
+<script >
+
+// import About from '../../components/Profile/About.vue'
+// import Photos from '../../components/Profile/Photos.vue'
+// import Videos from '../../components/Profile/Videos.vue'
+// import Post from '../../components/Profile/Post.vue'
+// import Collections from '../../components/Profile/Collections.vue'
+
+export default{
+
+components:{
+
+
+},
+
+data(){
+
+	return {
+
+
+	}
+},
+
+
+
+
+
+}
+	
+
+
+</script>
